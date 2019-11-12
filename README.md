eb04efd7697d39a02d7426eec99bbaaa4b231a01
Mauro S. Oddi <mauro.oddi@gmail.com>
2019-11-12 14:54:45 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# THT templates to deploy RHOSP11 with 2 Networker nodes in virtual lab
---
+++ overcloud_deploy-osp11.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+THT=/home/stack/templates/
+N_COMP=3
+N_CTRL=3
+
+time openstack overcloud deploy \
+     --templates /usr/share/openstack-tripleo-heat-templates \
+     -r ${THT}/templates/custom-roles-data.yaml \
+     -e ${THT}/templates/00-env-common.yaml \
+     -e ${THT}/templates/10-network-env.yaml \
+     -e ${THT}/templates/11-hostname-map.yaml \
+     -e ${THT}/templates/12-fixed-ips.yaml \
+     -e ${THT}/templates/20-networker-role.yaml \
+     -e ${THT}/templates/21-generic-role.yaml \
+     -e ${THT}/templates/30-storage-env.yaml \
+     -e ${THT}/templates/40-hiera-custom.yaml \
+     -e ${THT}/templates/41-unversioned-notifications.yaml \
+     -e ${THT}/templates/50-fernet-tokens.yaml
+
